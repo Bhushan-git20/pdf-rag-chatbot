2ebef1577472c01f4833e1a7e129f6572fa8f368
Bhushan <bhush@mindmate.app>
2026-05-25 19:57:11 +0530

    feat: migrate to modern LangChain v0.3 text splitters and huggingface package imports

--- utils/pdf_processor.py
+++ utils/pdf_processor.py
@@ -1,7 +1,7 @@
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 import streamlit as st
 
 
